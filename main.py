f17c118d1a3def1527e49f38ddfa117f64434b01
River Dyke <river@xenia.river.fuck>
2026-06-14 16:30:34 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from sklearn.ensemble import RandomForestClassifier
+import pandas
+
+statsCSV = pandas.read_csv("PlayerStatistics.csv", index_col="personId", nrows=120, usecols=["personId", "win", "plusMinusPoints"])
+
+print(statsCSV)
+
+statsMatrix = statsCSV.to_numpy()
+trainMatrix = statsMatrix[31:120]
+predictMatrix = statsMatrix[:30]
+clf = RandomForestClassifier(random_state=0)
+
+X = [[ 1,  2,  3],  # 2 samples, 3 features
+
+     [11, 12, 13]]
+
+y = [0, 1]  # classes of each sample
+
+print(clf.fit(trainMatrix, y))
+print(clf.predict(trainMatrix))  # predict classes of the training data
+print(clf.predict(predictMatrix))  # predict classes of new data
