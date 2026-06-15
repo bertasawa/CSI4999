721192843f0a7af236f216a133908ef2af3106c6
River Dyke <river@xenia.river.fuck>
2026-06-14 20:06:33 -0400

    basic api

--- server.py
+++ server.py
@@ -9,11 +9,11 @@ def hello_world():
 @app.route('/player/')
 def player():
 	name = request.args.get('name') #1630595
-	statsCSV = pandas.read_csv("../bball-reference-datasets/Data/Player Per Game.csv", usecols=["player", "team", "pts_per_game"])
+	statsCSV = pandas.read_csv("../bball-reference-datasets/Data/Player Per Game.csv", usecols=["player", "team", "pts_per_game", "trb_per_game", "ast_per_game"])
 	print(f"{name}")
 	print(statsCSV.query("player == @name"))
 
-	return Response(statsCSV.query("player == @name").to_json(), mimetype='text/json')
+	return Response(statsCSV.query("player == @name").head(1).to_json(), mimetype='text/json')
 
 if __name__ == '__main__':
     app.run(port=1234, threaded=True)
