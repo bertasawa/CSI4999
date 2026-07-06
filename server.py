ee34b2f510dae4e55d9cddd97ba156e384b499f9
bertasawa <bertasawa@yahoo.com>
2026-07-05 23:09:51 -0400

    Enable CORS and fix prediction response

--- server.py
+++ server.py
@@ -1,53 +1,91 @@
-from flask import Flask, Response, request, current_app, abort
+from flask import Flask, Response, request, jsonify
+from flask_cors import CORS
 import pandas
-import json
 import main
 
 app = Flask(__name__)
 
-@app.route('/')
+# Allows the React frontend to call the Flask backend
+CORS(app)
+
+
+@app.route("/")
 def hello_world():
-	return("hello world")
+    return "hello world"
 
-@app.route('/player/')
+
+@app.route("/player/")
 def player():
-	name = request.args.get('name') #1630595
-	statsCSV = pandas.read_csv("../bball-reference-datasets/Data/Player Per Game.csv", usecols=["player", "team", "pts_per_game", "trb_per_game", "ast_per_game"])
-	print(f"{name}")
-	print(statsCSV.query("player == @name"))
+    name = request.args.get("name")
 
-	return Response(statsCSV.query("player == @name").head(1).to_json(), mimetype='text/json')
+    statsCSV = pandas.read_csv(
+        "../bball-reference-datasets/Data/Player Per Game.csv",
+        usecols=[
+            "player",
+            "team",
+            "pts_per_game",
+            "trb_per_game",
+            "ast_per_game",
+        ],
+    )
 
-@app.route('/team/')
+    print(name)
+    print(statsCSV.query("player == @name"))
+
+    return Response(
+        statsCSV.query("player == @name").head(1).to_json(),
+        mimetype="application/json",
+    )
+
+
+@app.route("/team/")
 def team():
-	name = request.args.get('name')
-	teamCSV = pandas.read_csv("../bball-reference-datasets/Data/Team Summaries.csv", usecols=["season", "abbreviation", "w","l"])
-	res = teamCSV.query("abbreviation == @name").query("season == 2026")
-	print(res)
-	return Response(res.to_json(), mimetype='text/json')
+    name = request.args.get("name")
 
-@app.route('/predict/')
+    teamCSV = pandas.read_csv(
+        "../bball-reference-datasets/Data/Team Summaries.csv",
+        usecols=["season", "abbreviation", "w", "l"],
+    )
+
+    res = teamCSV.query("abbreviation == @name").query("season == 2026")
+
+    print(res)
+
+    return Response(
+        res.to_json(),
+        mimetype="application/json",
+    )
+
+
+@app.route("/predict/")
 def predict():
-	name1 = request.args.get('name1')
-	name2 = request.args.get('name2')
-	predict1 = main.predictTeam(name1)
-	predict2 = main.predictTeam(name2)
-	score = predict1 - predict2
-	winner = ''
-	if score > 0:
-		winner = name1
-	elif score < 0:
-		winner = name2
-	else:
-		winner = "Tie"
-	scorePercent = 1-((1-(abs(score)))/2)
-	res = [winner, scorePercent]
-	print(predict1)
-	print(predict2)
-	print(score)
-	print(scorePercent)
-	return(res)
+    name1 = request.args.get("name1")
+    name2 = request.args.get("name2")
+
+    if not name1 or not name2:
+        return jsonify({"error": "Two team names are required"}), 400
+
+    predict1 = main.predictTeam(name1)
+    predict2 = main.predictTeam(name2)
+
+    score = predict1 - predict2
+
+    if score > 0:
+        winner = name1
+    elif score < 0:
+        winner = name2
+    else:
+        winner = "Tie"
+
+    scorePercent = 1 - ((1 - abs(score)) / 2)
+
+    print("Team 1 prediction:", predict1)
+    print("Team 2 prediction:", predict2)
+    print("Score difference:", score)
+    print("Winning percentage:", scorePercent)
+
+    return jsonify([winner, scorePercent])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(port=1234, threaded=True)
